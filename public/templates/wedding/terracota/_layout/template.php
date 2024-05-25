2ffdb80ca36e70fbc405cec6164ee429917ec65c
Juan Kuga <juankp3@gmail.com>
2024-05-25 18:08:08 -0500

    fix: secction Recepción

--- public/templates/wedding/terracota/_layout/template.php
+++ public/templates/wedding/terracota/_layout/template.php
@@ -20,9 +20,9 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-	<link rel="stylesheet" href="<?php echo BASE_URL ?>/public/dist/css/terracota.min.css?v=25">
+	<link rel="stylesheet" href="<?php echo BASE_URL ?>/public/dist/css/terracota.min.css?v=26">
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Incluye jQuery -->
-	<script type="module" src="<?php echo BASE_URL ?>/public/dist/js/bundle.min.js?v=25"></script>
+	<script type="module" src="<?php echo BASE_URL ?>/public/dist/js/bundle.min.js?v=26"></script>
 	<title><?php echo $title; ?></title>
 </head>
 
