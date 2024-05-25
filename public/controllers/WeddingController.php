42118f0df5557ff56e55f4d0c96f529da269ac63
Juan Kuga <juankp3@gmail.com>
2024-05-25 14:13:49 -0500

    feat: Cambia el codigo de la version

--- public/controllers/WeddingController.php
+++ public/controllers/WeddingController.php
@@ -8,28 +8,15 @@ class WeddingController
 
     public function terracota()
     {
-        $params['title'] = 'Usuarios';
+        $params['title'] = 'Angelica y Luis';
 
         Flight::set('flight.views.path', 'public/templates/wedding/terracota');
         Flight::render('index', $params, 'body_content');
         Flight::render('_layout/template');
     }
-
-    public function dist()
-    {
-		// echo "asda";
-        // $this->params['title'] = 'Usuarios';
-
-        Flight::set('flight.views.path', 'public/dist/');
-        // Flight::render('index', $this->params, 'body_content');
-        // Flight::render('_layout/template');
-    }
-
-
 }
 
 $oWeddingController = new WeddingController();
 Flight::route('GET /boda', array($oWeddingController, 'index'));
 Flight::route('GET /boda/angelica-y-luis', array($oWeddingController, 'terracota'));
 Flight::route('GET /boda/ladislao-y-luis', array($oWeddingController, 'rouse'));
-Flight::route('GET /dist/', array($oWeddingController, 'dist'));
--- public/templates/wedding/terracota/_layout/template.php
+++ public/templates/wedding/terracota/_layout/template.php
@@ -1,34 +1,33 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <script src="<?php echo JS_URL ?>/vendor.bundle.js"></script>
-    <script src="<?php echo JS_URL ?>/theme.bundle.js"></script>
-    <script type="text/javascript">
-    var app = {
-        'urls' : {
-        'base_url' : '<?php echo BASE_URL ?>',
-        'js_url' : '<?php echo JS_URL ?>',
-        'img_url' : '<?php echo IMG_URL ?>'
-        }
-    }
-    </script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<script type="text/javascript">
+		var app = {
+			'urls': {
+				'base_url': '<?php echo BASE_URL ?>',
+				'js_url': '<?php echo JS_URL ?>',
+				'img_url': '<?php echo IMG_URL ?>'
+			}
+		}
+	</script>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+	<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="<?php echo BASE_URL ?>/public/dist/css/terracota.min.css?v=19">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Incluye jQuery -->
-    <script type="module" src="./dist/js/bundle.min.js?v=15"></script>
-    <title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+	<link rel="stylesheet" href="<?php echo BASE_URL ?>/public/dist/css/terracota.min.css?v=20">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Incluye jQuery -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+	<script type="module" src="<?php echo BASE_URL ?>/public/dist/js/bundle.min.js?v=20"></script>
+	<title><?php echo $title; ?></title>
 </head>
+
 <body class="body-envelope">
-    <?php echo $body_content; ?>
+	<?php echo $body_content; ?>
 </body>
+
 </html>
-
-
