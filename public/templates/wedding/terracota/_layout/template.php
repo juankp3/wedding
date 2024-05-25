01fa451d99b4acac1563938a700ed761e3760ef6
Juan Kuga <juankp3@gmail.com>
2024-05-25 14:21:36 -0500

    feat: Cambia la version de los archivos

--- public/templates/wedding/terracota/_layout/template.php
+++ public/templates/wedding/terracota/_layout/template.php
@@ -17,13 +17,13 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 	<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="<?php echo BASE_URL ?>/public/dist/css/terracota.min.css?v=24">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Incluye jQuery -->
-    <script type="module" src="./dist/js/bundle.min.js?v=15"></script>
-    <title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="<?php echo BASE_URL ?>/public/dist/css/terracota.min.css?v=20">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Incluye jQuery -->
+	<script type="module" src="<?php echo BASE_URL ?>/public/dist/js/bundle.min.js?v=20"></script>
+	<title><?php echo $title; ?></title>
 </head>
 
 <body class="body-envelope">
