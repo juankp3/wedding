6f54d7a7d8ac0f62cf814230eb6f15dfe292df56
Juan Kuga <juankp3@gmail.com>
2024-06-03 15:07:06 -0500

    feat: modal

--- public/templates/code/_partials/modal.php
+++ public/templates/code/_partials/modal.php
@@ -10,19 +10,50 @@
 				<div class="container-fluid">
 					<div class="row">
 						<div class="col-sm-12">
-							2 Pases
 							<div class="row">
-								<div class="col-8 col-sm-6">Level 2: .col-8 .col-sm-6</div>
-								<div class="col-4 col-sm-6">Level 2: .col-4 .col-sm-6</div>
+								<div class="col-6 col-sm-6"><b>Ultima visita:</b> fecha</div>
+								<div class="col-6 col-sm-6"><b>Cant. de vistas:</b> 2</div>
 							</div>
+							<div class="row">
+								<div class="col-6 col-sm-6"><b>Ultima visita:</b> fecha</div>
+								<div class="col-6 col-sm-6"><b>Cant. de vistas:</b> 2</div>
+							</div>
+							<div class="row">
+								<div class="col-6 col-sm-6"><b>Ultima visita:</b> fecha</div>
+								<div class="col-6 col-sm-6"><b>Cant. de vistas:</b> 2</div>
+							</div>
+
 						</div>
 					</div>
+
 					<div class="row">
-						<div class="col-md-4">.col-md-4</div>
-						<div class="col-md-4 ms-auto">.col-md-4 .ms-auto</div>
+						<h6>Invitados adicionales</h6>
+						<table class="table">
+							<thead>
+								<tr>
+									<th scope="col">#</th>
+									<th scope="col">First</th>
+									<th scope="col">Last</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>Mark</td>
+									<td>Otto</td>
+								</tr>
+								<tr>
+									<th scope="row">2</th>
+									<td>Jacob</td>
+									<td>Thornton</td>
+								</tr>
+
+							</tbody>
+						</table>
 					</div>
+
 				</div>
-				...
+
 			</div>
 
 			<!-- <div class="modal-footer">
