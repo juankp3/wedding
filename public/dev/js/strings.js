6a2c63d31020a2044d98900c9c1fdaf03747cbe7
ladislao <ladislaox@gmail.com>
2024-06-06 21:02:39 -0500

    fix: cambio boton asistencia wsp. Nuevas fotosanimacion de sobre

--- public/dev/js/strings.js
+++ public/dev/js/strings.js
@@ -1,4 +1,4 @@
-const titleConfirm = 'Gracias por confirmar tu asistencia x2'
-const descriptionConfirm = 'Por favor presente este codigo QR en la entrada del evento x2'
+const titleConfirm = 'Gracias por confirmar tu asistencia'
+const descriptionConfirm = 'Por favor presenta este codigo QR en la entrada del evento'
 const descriptionCancel = 'Nos deja muy triste el que no puedas asistir al evento mas importante de nuestra vida. Y aunque nos falte tu presencia que no falte tu regalo.'
 const anchorAccouts =  'Mostrar los numeros de cuenta'
--- public/templates/wedding/terracota/widgets/attendance.php
+++ public/templates/wedding/terracota/widgets/attendance.php
@@ -2,9 +2,12 @@
 
 	<div class="attendance__top">
 		<h2>Confirma </br> tu Asistencia</h2>
+		<p>con nuestra Wedding Planner</p>
 	</div>
 
+
 	<div class="attendance__contact">
+		<?php /* ?>
 		<div class="contact__container contact__container--bride">
 			<a href="https://walink.co/d69cc7" class="button button__primary">
 				<i class="button__whatsapp fa-brands fa-whatsapp"></i>
@@ -15,6 +18,14 @@
 			<a href="https://walink.co/841426" class="button button__primary">
 				<i class="button__whatsapp fa-brands fa-whatsapp"></i>
 				Novio
+			</a>
+		</div>
+		<?php */ ?>
+
+		<div class="contact__container contact__container--weddingplanner">
+			<a href="https://bit.ly/3z0FedF" class="button button__primary">
+				<i class="button__whatsapp fa-brands fa-whatsapp"></i>
+				Wedding Planeer
 			</a>
 		</div>
 	</div>
